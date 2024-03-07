1fec924ec4f4bfcbcfaab7a5464b9b1ce6c26082
msidiq2235 <muhsidiq2005@gmail.com>
2024-03-07 19:21:39 +0700

    <Prosedur2>

--- Pertemuan3/Pertemuan3.cpp
+++ Pertemuan3/Pertemuan3.cpp
@@ -26,4 +26,21 @@ void input() {
 
 }
 
+void bubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1; // step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+			if (arr[j] > arr[j + 1]) { // step 3
+				int temp = arr[j];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1; //step 4
+
+		cout << endl;
+
+	} while (pass <= n - 1); //step 5
+}
+
 
